1ca269586e7118d2b56604a872a651df3db771a7
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-06-28 15:30:51 +0100

    listed by most viewed passed

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,10 +5,18 @@ class Parser
     @log = []
   end
 
+  def display_by_most_views
+    save_log
+    views = log.each {|log| log.slice!(-17..100)}
+    views.each_with_object(Hash.new(0)){ |m,h| h[m] += 1}.sort_by{ |k,v| v}.reverse
+  end
+
+private
+
   def save_log
     File.open("/Users/ScottGledhill/desktop/projects/webpage_log/lib/webserver.log", "r") do |file_handle|
       file_handle.each_line do |data|
-      @log << data
+        @log << data
       end
     end
   end
