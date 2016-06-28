959e9df089d22e6371b41b9b259bf505c7d79793
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-06-28 12:51:59 +0100

    add sorted list test

--- lib/parser.rb
+++ lib/parser.rb
@@ -5,7 +5,7 @@ class Parser
     @log = []
   end
 
-  def access_log
+  def save_log
     File.open("/Users/ScottGledhill/desktop/projects/webpage_log/lib/webserver.log", "r") do |file_handle|
       file_handle.each_line do |data|
       @log << data
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,15 +3,19 @@ require "parser"
 describe Parser do
   subject(:parser) {described_class.new}
 
-	describe 'log' do
-		it 'can display data' do
-      parser.access_log
+	describe 'parser' do
+		it 'can display log data' do
+      parser.save_log
 			expect(parser.log).to include("/help_page/1 126.318.035.038\n")
 		end
 
-    it 'can display data' do
-      parser.access_log
+    it 'can display log data' do
+      parser.save_log
       expect(parser.log).to include("/home 200.017.277.774\n")
+    end
+
+    it 'can list webpages by most views' do
+      expect(parser.display_by_most_views).to eq "add sorted list here"
     end
   end
 end
